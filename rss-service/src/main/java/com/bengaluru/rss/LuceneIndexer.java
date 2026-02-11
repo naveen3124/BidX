3dd29b1d777bf4ccc455fe960eb1408f07b055ca
naveen <naveenvit3124@gmail.com>
2026-02-12 01:28:35 +0530

    lucene index modifications

--- rss-service/src/main/java/com/bengaluru/rss/LuceneIndexer.java
+++ rss-service/src/main/java/com/bengaluru/rss/LuceneIndexer.java
@@ -20,6 +20,10 @@ import org.apache.lucene.document.Field;
 import org.apache.lucene.document.LongPoint;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
+import org.apache.lucene.document.StoredField;
+import org.apache.lucene.document.NumericDocValuesField;
+import org.apache.lucene.document.SortedDocValuesField;
+import org.apache.lucene.util.BytesRef;
 
 public final class LuceneIndexer {
 
@@ -38,17 +42,31 @@ public final class LuceneIndexer {
 
     public void index(FeedEntry e) throws Exception {
         Document d = new Document();
+
+        long ts = e.publishedAt().toEpochMilli();
+
         d.add(new StringField("id", e.id(), Field.Store.YES));
         d.add(new StringField("source", e.source(), Field.Store.YES));
+        d.add(new SortedDocValuesField("source",
+                new BytesRef(e.source())));
+
         d.add(new TextField("title", e.title(), Field.Store.YES));
-        d.add(new TextField("summary", e.summary(), Field.Store.NO));
+        d.add(new TextField("summary", e.summary(), Field.Store.YES));
+
         d.add(new StringField("link", e.link(), Field.Store.YES));
-        d.add(new LongPoint("published", e.publishedAt().toEpochMilli()));
+
+        d.add(new LongPoint("published", ts));
+        d.add(new StoredField("published", ts));
+        d.add(new NumericDocValuesField("published", ts));
 
         writer.updateDocument(new Term("id", e.id()), d);
     }
 
     public void commit() throws Exception {
         writer.commit();
+    }
+    
+    public void close() throws Exception {
+        writer.close();
     }
 }
